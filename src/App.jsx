4e91d6f1ec5d03c211dbc29bbd27d19cf804ef90
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-09-03 16:35:18 -0500

    App context wrapping whole App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import SchoolCatalog from "./SchoolCatalog";
 import Header from "./Header";
 import ClassSchedule from "./ClassSchedule";
+import { CourseProvider } from "./CourseContext";
 
 export default function App() {
   return (
-    <div>
+    <CourseProvider>
       <Header />
       <SchoolCatalog />
       <ClassSchedule />
-    </div>
+    </CourseProvider>
   );
 }
