02fa834ae8493638347df6e37c8e082a45b0805f
PuzzleEmptyM <emptympuzzle@gmail.com>
2024-09-03 16:35:05 -0500

    Header is updating correctly

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,17 @@
 import logo from "./assets/logo.png";
+import React, { useContext } from 'react';
+import { CourseContext } from './CourseContext';
+
 
 export default function Header() {
+  const { enrolledCourses } = useContext(CourseContext);
+
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
+      <div className="enrollment">
+      <p>Classes Enrolled: {enrolledCourses.length}</p>
+      </div>
     </div>
   );
 }
